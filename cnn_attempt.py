c7c95ba40c0a21cd9a3718483dcce8e364c216b9
Kassandra Williams <kassandra.williams.22@cnu.edu>
2025-03-30 18:13:45 -0400

    Created cnn attempt

--- cnn_attempt.py
+++ cnn_attempt.py
@@ -167,3 +167,4 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(data, labels.T, test_size=0.2, random_state=42)
     train_network(model, X_train, y_train, args.model, epochs=5, save_flag=True)
     evaluate_model(model, X_test, y_test)
+
