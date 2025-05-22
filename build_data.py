fbe009d730e29a8854d73f74bd89b2af164384ea
Kassandra Williams <kassandra.williams.22@cnu.edu>
2025-05-21 22:27:40 -0400

    Typed out layout/plan for generating PNGs of varying sizes.

--- build_data.py
+++ build_data.py
@@ -8,65 +8,81 @@ from PIL import Image
 import tensorflow as tf
 import io
 import os
-# function takes in matrix size, range of block sizes, noise, and de-noise
+
+'''
+Function to generate matrices.
+Parameters: 
+    - matrix_size: The size of the matrix.
+    - block_sizes: The list of block sizes.
+    - noise: How much noise you would like to add.
+    - de_noise: How much noise to remove, or "denoise"
+Function returns the generated matrix.
+'''
 def generate_data(matrix_size, block_sizes, noise=0.2, de_noise=0.4):
 
-    # initialize a numpy array of all zeros
+    # Initialize a numpy array of all zeros
     matrix = np.zeros((matrix_size, matrix_size))
 
-    # in order to add/remove noise later, and avoid the blocks, we need to create a mask
+    # In order to add/remove noise later, and avoid the blocks, we need to create a mask
     block_mask = np.zeros_like(matrix, dtype=bool)
 
-    # starting pos
+    # Starting position
     row, col = 0, 0
 
-    # first block: set the entries in the blocks along the diagonal to 1
+    # First block: set the entries in the blocks along the diagonal to 1
     for block in block_sizes:
         if row + block > matrix_size or col + block > matrix_size:
             break # don't let block exceed matrix boundaries
 
-        # fill block with ones
+        # Fill block with ones
         matrix[row:row+block, col:col+block] = 1
         block_mask[row:row+block, col:col+block] = True # also add to block_mask
 
-        # move diagonally
+        # Move diagonally
         row += block
         col += block
 
-    # second block: look only at entries that are 1 and flip them to a 0 with a probability
+    # Second block: look only at entries that are 1 and flip them to a 0 with a probability
     # related to the "de_noise" parameter
     denoise_mask = np.random.rand(*matrix.shape) < de_noise # Creates a boolean mask
     matrix[denoise_mask] = 0 # Sets these "de_noise" positions to a 0
 
-    # third block: look only at the entries that are 0 (preferably not in the original block space)
+    # Third block: look only at the entries that are 0 (preferably not in the original block space)
     # and flip them to a 1 with probability related to the "noise" parameter
     noise_mask = np.random.rand(*matrix.shape) < noise
 
     # Combine noise_mask and block_mask to ensure we avoid the block positions
     noise_mask = np.logical_and(noise_mask, ~block_mask)
 
-    # apply noise to matrix
-    # these values should be from 0-0.5
+    # Apply noise to matrix
+    # These values should be from 0-0.5
     matrix[noise_mask] = 1
 
     return matrix
 
-# generate multiple matrices of a specific size, giving uniform block sizes
+'''
+Function will generate multiple matrices of a specified size.
+Parameters:
+    - matrix_size: The size of the matrix.
+    - amount: The amount of matrices to generate. Our sample size.
+Function will save the matrices to h5 file. 
+Returns an array of matrices.
+'''
 def generate_multiple_uniform(matrix_size, amount=1000):
-    # we will go by a general rule that max noise=0.3 and max denoise=0.4
+    # We will go by a general rule that max noise=0.3 and max denoise=0.4
     # in order to still have "visible" blocks
     array_of_matrices = np.empty((matrix_size, matrix_size, amount))
     block_size_array = []
     for i in range(amount):
-        # random block ratio
+        # Random block ratio
         block_ratio = np.random.uniform(0.05, 0.1)
         #print(block_ratio)
 
-        #generate block sizes for current matrix
+        # Generate block sizes for current matrix
         blocks, block_size = generate_fixed_blocks(matrix_size, block_ratio=block_ratio)
         block_size_array.append(block_size)
 
-        # generate the matrix
+        # Generate the matrix
         array_of_matrices[:, :, i] = generate_data(matrix_size, blocks, noise=0.2, de_noise=0.2)
 
     #blocks_array = np.array(blocks)
@@ -74,7 +90,7 @@ def generate_multiple_uniform(matrix_size, amount=1000):
 
 
 
-        # SAVE THE NEW MATRIX TO AN H5 FILE
+    # SAVE THE NEW MATRIX TO AN H5 FILE
     with h5py.File('synthetic_data.h5', 'a') as f:
 
         matrixset_name = 'matrix_of_' + str(matrix_size)
@@ -114,6 +130,13 @@ def generate_block_sizes(matrix_size, block_size_range=(0.05, 0.07)):
 
     return blocks # our list of block sizes
 
+'''
+Function generates a list of fixed block sizes.
+Parameters:
+    - matrix_size: The size of the matrix.
+    - block_ratio: Ratio of block to matrix, i.e. what percentage of matrix size will our blocks be
+Function returns the list of blocks and the size.
+'''
 def generate_fixed_blocks(matrix_size, block_ratio=0.07):
     block_size = int(matrix_size * block_ratio)
     #print(f"The uniform block size is {block_size}")
@@ -125,35 +148,88 @@ def generate_fixed_blocks(matrix_size, block_ratio=0.07):
         blocks.append(block_size)
     return blocks, block_size
 
-# def save_to_h5(matrix, file_name='synthetic_data.h5', dataset_name):
-#     # SAVE THE NEW MATRIX TO AN H5 FILE
-#     with h5py.File(file_name, 'a') as f:
-#
-#         matrixset_name = dataset_name
-#         f.create_dataset(matrixset_name, data=matrix)
-#
-#         labelset_name = 'labels' + str(num_matrix)
-#         f.create_dataset(labelset_name, data=block_sizes)
-#     print("Matrix saved to 'synthetic_data.h5'")
+'''
+Function converts matrix to png format
+Parameters:
+    - matrix: The matrix to convert.
+Returns the PIL image object.
+'''
+def matrix_to_png(matrix):
+    # 255 is white, 0 is black.
+    # when we use matplotlib to show matrices, we view the 1s as black so we need to invert matrix
+    matrix = (1- matrix) * 255
+    matrix = matrix.astype(np.uint8)
+    img = Image.fromarray(matrix)
+    return img
+
+'''
+Function stores the pngs to a file directory.
+Parameters:
+    - data: The array of matrices.
+    - labels: The block sizes.
+    - width: The width of the pngs. By default, pass the matrix size.
+    - height: The height of the pngs. By default, pass the matrix size.
+    - base_dir: Where to store the pngs.
+'''
+def store_pngs(data, labels, width, height, base_dir='png_dataset'):
+    os.makedirs(base_dir, exist_ok=True)
+    matrix_size = data[:, :, 0].shape[0]
+    size_folder = os.path.join(base_dir, f'size_{matrix_size}')
+    os.makedirs(size_folder, exist_ok=True)
+    for label in labels:
+        label_folder = os.path.join(size_folder, f'label_{label}')
+        os.makedirs(label_folder, exist_ok=True)
+    for i in range(data.shape[2]):
+        matrix = data[:,:,i]
+        img = matrix_to_png(matrix)
+        if (width != matrix_size) or (height != matrix_size):
+            img = img.resize((width, height))
+        file_path = os.path.join(size_folder, f'label_{labels[i]}',f'matrix_{i}.png')
+        img.save(file_path)
+
+def generate_acceptable_blocks(matrix_size):
+    return
+'''
+This function will generate varying matrix sizes
+
+It will save the matrices as PNGs of a given dimension
+Parameters: 
+    - Number of matrix sizes you want to generate
+    - Range of matrix sizes allowed
+    - Sample amount for each matrix size (i.e. 1000 samples of size 64)
+    
+Each set of matrices for specified size will have a set of "allowable" block sizes
+This will call a function that will return a list of acceptable block sizes based on the size
+
+(i.e. a matrix of size 1000 could have blocks of size 2, 5, 10, 20, 50, 100, 125, 200, 250...
+ We probably don't need all of those, so instead will will base it off percentages. i.e. 2%, 5%, 10%, 15%, 20%
+ In reality matrices may be extremely large and have a larger range of block sizes but for now we will keep it simple)
+ 
+1. Generate list of matrix sizes
+2. Generate list of acceptable block sizes for said matrix size
+3. For x amount of times (specified sample amount), a random choice from the list of acceptable block sizes is chosen
+4. Calls generate_data and generates an array of matrices with that specified block size
+5. Since we are resizing the PNG, I will change the store_pngs function by adding a parameter for width and height
+    - If you do not wish to resize the image, simply pass the matrix size for width and height
+'''
+def generate_varying_matrices(matrix_size, amount=1000, size_range=(100,500)):
+    return
 
 
-# test out matrix
-#size = 50
-#blocks, block_size = generate_fixed_blocks(size)
-#matrices = generate_multiple_uniform(size)
-#handle = h5py.File('./synthetic_data.h5', 'r+')
-#data = np.array(handle['matrix_of_50'])
 
-#handle.close()
-#matplotlib.pyplot.spy(data[:,:,50])
-#matplotlib.pyplot.spy(data[:,:,1])
-#matplotlib.pyplot.show()
 
-##******************************************************************************************##
-##******************************************************************************************##
-##******************************************************************************************##
-##******************************************************************************************##
-## **************** HARD CODED SECTION *****************************************************##
+
+
+
+
+
+# Resizing PNGS:
+# image = Image.open("path/to/your/image.jpg")
+# new_width = 500
+# new_height = 300
+# resized_image = image.resize((new_width, new_height))
+
+#***************************************************** HARD CODED SECTION *****************************************************#
 hard_size=64
 hard_amount=5000
 array_of_matrices = np.empty((hard_size, hard_size, hard_amount))
@@ -180,28 +256,8 @@ for i in range(hard_amount):
     if (i == 0):
         print(array_of_matrices[:,:,i])
 
-# CONVERT TO PNG
-def matrix_to_png(matrix):
-    # 255 is white, 0 is black.
-    # when we use matplotlib to show matrices, we view the 1s as black so we need to invert matrix
-    matrix = (1- matrix) * 255
-    matrix = matrix.astype(np.uint8)
-    img = Image.fromarray(matrix)
-    return img
-#save
-def store_pngs(data, labels, base_dir='png_dataset'):
-    os.makedirs(base_dir, exist_ok=True)
-    matrix_size = data[:, :, 0].shape[0]
-    size_folder = os.path.join(base_dir, f'size_{matrix_size}')
-    os.makedirs(size_folder, exist_ok=True)
-    for label in labels:
-        label_folder = os.path.join(size_folder, f'label_{label}')
-        os.makedirs(label_folder, exist_ok=True)
-    for i in range(data.shape[2]):
-        matrix = data[:,:,i]
-        img = matrix_to_png(matrix)
-        file_path = os.path.join(size_folder, f'label_{labels[i]}',f'matrix_{i}.png')
-        img.save(file_path)
+
+
 data = np.array(array_of_matrices)
 store_pngs(data, block_size_array)
 
@@ -211,25 +267,3 @@ store_pngs(data, block_size_array)
 matplotlib.pyplot.spy(data[:,:,0])
 matplotlib.pyplot.show()
 
-
-
-# added comment to test commit
-
-
-
-
-# # SAVE THE NEW MATRIX TO AN H5 FILE
-# with h5py.File('synthetic_data.h5', 'w') as f:
-#     matrixset_name = 'matrix_of_hard_64'
-#     f.create_dataset(matrixset_name, data=array_of_matrices)
-#
-#     labelset_name = 'labels_for_hard_64'
-#     f.create_dataset(labelset_name, data=block_size_array)
-#
-#     data = np.array(f[matrixset_name])
-#     labels = np.array(f[labelset_name])
-#     # print(data.shape)
-#     # print(labels.shape)
-#
-# print("Matrices of size " + str(hard_size) + " saved to 'synthetic_data.h5'")
-# # print("Block sizes of matrices: " + str(matrix_size))
