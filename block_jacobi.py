7fb96ef8139dc51a1a2bf7134403170fbb9505d5
Kassandra Williams <kwilli36@mail.umw.edu>
2025-07-02 22:11:32 -0400

    Png_builder generates sparse matrices, calls function from new block_jacobi file

---
+++ block_jacobi.py
@@ -0,0 +1,104 @@
+from scipy.sparse.linalg import gmres
+from scipy.sparse import bmat, csc_matrix
+from scipy.sparse.linalg import inv
+import time
+
+def block_jacobi_preconditioner(A, block_size):
+    """
+    Creates a block Jacobi preconditioner for a sparse matrix A.
+
+    Args:
+        A (scipy.sparse.csr_matrix): The input sparse matrix.
+        block_size (int): The size of the blocks.
+
+    Returns:
+        scipy.sparse.csr_matrix: The block Jacobi preconditioner matrix.
+    """
+    n = A.shape[0]
+    # num_blocks = n // block_size
+    # if n % block_size != 0:
+    #     raise ValueError("Matrix size must be divisible by block_size")
+
+    inv_blocks = []
+    for row_start in range(0, n, block_size):
+        row_end = min(row_start + block_size, n)
+        block = A[row_start:row_end, row_start:row_end]
+        block = csc_matrix(block)
+        inv_blocks.append(inv(block))
+
+    # Create the block diagonal preconditioner matrix
+    num_blocks = len(inv_blocks)
+    M = bmat([[inv_blocks[i] if i == j else None for j in range(num_blocks)] for i in range(num_blocks)], format="csr")
+    return M
+
+class gmres_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+        self.rk = 0
+        self.residuals = []
+    def __call__(self, rk=None):
+        self.niter += 1
+        self.rk = rk
+        self.residuals.append(self.rk)
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
+
+
+def find_best_block_size(n, A, b, eval_key='run_time'):
+    pre_iters = {}
+    counter = gmres_counter(disp=False)
+
+    # Original run time with no preconditioner
+    start_time = time.perf_counter()
+    x, i_exitCode = gmres(A, b, rtol=1e-2, callback=counter, maxiter=int(1e6))
+    end_time = time.perf_counter()
+    run_time = end_time - start_time
+    #print(f"Original run time: {run_time}")
+    #print(f"Original number of iterations: {counter.niter}")
+    #print(f"rk is {counter.rk}")
+    print(f"Exit code is {i_exitCode}")
+    #plt.figure()
+    #plt.plot(counter.residuals)
+    #plt.show()
+
+    for divisor in range(1, n//2):
+        # print(f"Testing block size: {divisor}")
+        counter_pre = gmres_counter(disp=False)
+        # track time for M
+        start_time_for_M = time.perf_counter()
+        # track entire runtime
+        entire_start = time.perf_counter()
+        M = block_jacobi_preconditioner(A, divisor)
+        # M run time end
+        end_time_for_M = time.perf_counter()
+        # track time after M
+        start_after_M = time.perf_counter()
+
+
+
+        x_pre, exitCode = gmres(A, b, rtol=1e-2, callback=counter_pre,  M=M)
+        entire_end = time.perf_counter()
+        end_after_M = time.perf_counter()
+
+
+        M_run_time = end_time_for_M - start_time_for_M
+        run_after_M = end_after_M - start_after_M
+        entire_run = entire_end - entire_start
+        # print(f"M run time: {M_run_time}")
+        # print(f"Runtime after M: {run_after_M}")
+        # print(f"Entire run time: {entire_run}")
+        # print(f"Exit code: {exitCode}")
+        # print(f"rk is {counter_pre.rk}")
+        if exitCode == 0:
+            pre_iters[divisor] = {
+                'run_time': entire_run,
+                'iterations': counter_pre.niter
+                }
+
+    best_block = min(pre_iters, key=lambda k: pre_iters[k][eval_key])
+    #print("Made it through")
+    print(f"Best block size: {best_block} with {pre_iters[best_block]['iterations']} iterations and {pre_iters[best_block]['run_time']} run time.")
+    print(pre_iters)
+    return best_block
+
---
+++ png_builder.py
@@ -0,0 +1,187 @@
+import os
+import random
+import matplotlib
+import numpy as np
+import scipy.sparse as sp
+from PIL import Image
+from matplotlib import pyplot as plt
+from block_jacobi import find_best_block_size
+
+
+def generate_sparse(block_sizes, noise=0.1, de_noise=0.1, random_state=42):
+    rng = np.random.default_rng(random_state)
+    blocks = []
+    for block in block_sizes:
+        blocks.append(
+            sp.random(
+                block,
+                block,
+                density=1.0,
+                format="coo",
+                data_rvs=lambda k: rng.uniform(-1.0, 0.0, size=k),
+            )
+        )
+    A = sp.block_diag(blocks, format='coo')
+    n = A.shape[0]
+
+    if noise > 0.0 and A.nnz:
+        noise_mask = sp.random(
+            n,
+            n,
+            density=noise,
+            format="coo",
+            data_rvs=lambda k: rng.uniform(-1.0, 0.0, size=k),
+        )
+        A = A + noise_mask
+        A.sum_duplicates()
+    if de_noise > 0.0 and A.nnz:
+        A = A.tocoo()
+        # keep mask is boolean array, drops entires based on de_noise parameter
+        keep_mask = rng.random(A.nnz) >= de_noise
+        A = sp.coo_matrix(
+            (A.data[keep_mask],
+             (A.row[keep_mask], A.col[keep_mask])),
+            shape=A.shape)
+    A.setdiag(1)
+    return A.tocsr()
+
+def generate_acceptable_blocks(matrix_size):
+
+    block_list = []
+
+    # all divisors of matrix size
+    divisors = [d for d in range(1, matrix_size + 1) if matrix_size % d == 0]
+
+    # taking all divisors greater than 2% of matrix size but less than 30%
+    for divisor in divisors:
+        if (divisor) < (matrix_size * .30) and (divisor) > (matrix_size * .02):
+            block_list.append(divisor)
+
+
+    return block_list
+
+'''
+Function converts matrix to png format
+Parameters:
+    - matrix: The matrix to convert.
+Returns the PIL image object.
+'''
+def matrix_to_png(matrix):
+    # 255 is white, 0 is black.
+    # when we use matplotlib to show matrices, we view the 1s as black so we need to invert matrix
+    dense_bool = matrix.toarray() != 0
+    pixels     = (~dense_bool).astype(np.uint8) * 255
+    img = Image.fromarray(pixels, mode="L")
+    img.show()
+    return img
+
+'''
+Function stores the pngs to a file directory.
+Parameters:
+    - data: The array of matrices.
+    - labels: The block sizes.
+    - width: The width of the pngs. By default, pass the matrix size.
+    - height: The height of the pngs. By default, pass the matrix size.
+    - base_dir: Where to store the pngs.
+'''
+def store_pngs(data, labels, width, height, base_dir='png_dataset'):
+    os.makedirs(base_dir, exist_ok=True)
+    matrix_size = data[:, :, 0].shape[0]
+    size_folder = os.path.join(base_dir, f'size_{matrix_size}')
+    os.makedirs(size_folder, exist_ok=True)
+    for label in labels:
+        label_folder = os.path.join(size_folder, f'label_{label}')
+        os.makedirs(label_folder, exist_ok=True)
+    for i in range(data.shape[2]):
+        matrix = data[:,:,i]
+        img = matrix_to_png(matrix)
+        if (width != matrix_size) or (height != matrix_size):
+            img = img.resize((width, height))
+        file_path = os.path.join(size_folder, f'label_{labels[i]}',f'matrix_{i}.png')
+        img.save(file_path)
+
+
+'''
+This function will generate varying matrix sizes
+
+It will save the matrices as PNGs of a given dimension
+Parameters: 
+    - Number of matrix sizes you want to generate
+    - Range of matrix sizes allowed
+    - Sample amount for each matrix size (i.e. 1000 samples of size 64)
+
+Each set of matrices for specified size will have a set of "allowable" block sizes
+This will call a function that will return a list of acceptable block sizes based on the size
+
+(i.e. a matrix of size 1000 could have blocks of size 2, 5, 10, 20, 50, 100, 125, 200, 250...
+ We probably don't need all of those, so instead will will base it off percentages. i.e. 2%, 5%, 10%, 15%, 20%
+ In reality matrices may be extremely large and have a larger range of block sizes but for now we will keep it simple)
+
+1. Generate list of matrix sizes
+2. Generate list of acceptable block sizes for said matrix size
+3. For x amount of times (specified sample amount), a random choice from the list of acceptable block sizes is chosen
+4. Calls generate_data and generates an array of matrices with that specified block size
+5. Since we are resizing the PNG, I will change the store_pngs function by adding a parameter for width and height
+    - If you do not wish to resize the image, simply pass the matrix size for width and height
+'''
+
+
+def generate_varying_matrices(size_amount, sample_amount=1000, size_range=(100, 501)):
+    used_sizes = []
+    # we want size_amount of matrix sizes
+    for i in range(size_amount):
+        # pick the current size randomly, but I need it to be divisible by 10 for ease
+        this_size = random.choice(range(size_range[0], size_range[1], 10))
+        while this_size in used_sizes:
+            this_size = random.choice(size_range)
+        used_sizes.append(this_size)
+        print(f"Current size is: {this_size}")
+
+        # list of blocks for all matrices of specified size
+        best_size_array = []
+
+        # variable to hold our matrices
+        array_of_matrices = np.empty((this_size, this_size, sample_amount))
+
+        # generate our list of acceptable blocks for current size
+        acceptable_blocks = generate_acceptable_blocks(this_size)
+
+        # we want to generate sample_amount of matrices for each size
+        for j in range(sample_amount):
+            # list of blocks for our current matrix
+            blocks = []
+
+            # randomly pick a size from our list of acceptable blocks
+            block_size = random.choice(acceptable_blocks)
+
+            # the number of blocks
+            num_blocks = this_size // block_size
+
+            # add these blocks to list so we can generate matrix
+            for k in range(num_blocks):
+                blocks.append(block_size)
+
+            # generate the matrix
+            A = generate_sparse(blocks, noise=0.01, de_noise=0.01)
+            
+            array_of_matrices[:, :, j] = A 
+            if (j == 0):
+                print(array_of_matrices[:, :, j])
+            
+            n = A.shape[0]
+            b = np.ones(A.shape[0])
+            
+            best_size = find_best_block_size(n, A, b)
+            best_size_array.append(best_size)
+
+        data = np.array(array_of_matrices)
+        store_pngs(data, block_size_array, 500, 500) # storing PNGS as 500x500 for now
+
+        # print(pngs[0].size)
+        # pngs[0].show()
+        # compare to matplotlib image
+        #plt.spy(data[:, :, 0])
+        #plt.show()
+    return
+
+generate_varying_matrices(5, 1000, size_range=(100,500))
