9b7fcf6088a207c1a3728206d9bd42a489bb00e8
Kassandra Williams <kwilli36@mail.umw.edu>
2025-07-08 14:43:45 -0400

    Added print statement to png_builder

--- png_builder.py
+++ png_builder.py
@@ -184,7 +184,7 @@ def generate_varying_matrices(size_amount, sample_amount=1000, size_range=(100, 
         #plt.spy()
         #plt.show()
     return
-
+print(f"Acceptable blocks for size 128: {generate_acceptable_blocks(128)}")
 generate_varying_matrices(1, 1, size_range=(128,129))
 # acceptable_blocks = generate_acceptable_blocks(100)
 # blocks = []
